59e686437d90805ad2e4bfe001e6c439271de023
Jokūbas Toločka <jokubas.tj@gmail.com>
2021-04-27 18:23:42 +0100

    env variable update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React from "react";
 import UploadPhoto from "./UploadPhoto";
 import UploadDesign from "./UploadDesign";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="content">
-        <div>
-          <h1>Photos</h1>
-          <UploadPhoto />
-        </div>
-        <div>
-          <h1>Design</h1>
-          <UploadDesign />
-        </div>
+const App = () => (
+  <div className="App">
+    <div className="content">
+      <div>
+        <h1>Photos</h1>
+        <UploadPhoto />
+      </div>
+      <div>
+        <h1>Design</h1>
+        <UploadDesign />
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
